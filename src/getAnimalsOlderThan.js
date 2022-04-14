6b9f404ae93783ba50587a4f2fc99ac4ffffbe2f
Kevin Li <Kevinstart14@gmail.com>
2022-04-14 19:23:04 -0300

    resolvido 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const todosAnimais = data.species;
+// const idades = todosAnimais.residents.age
+// console.log(todosAnimais);
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const especie = todosAnimais.find((bicho) => bicho.name === animal);
+  const test = especie.residents.every((idade) => idade.age > age);
+  return test;
 }
+// console.log(getAnimalsOlderThan('lions', 1));
 
 module.exports = getAnimalsOlderThan;
