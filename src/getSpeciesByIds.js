4754bd7d00a28a1d03d8ecfe3a97fb5ae5d5af84
Kevin Li <Kevinstart14@gmail.com>
2022-04-14 19:59:41 -0300

    resolvido 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  const todos = [...ids];
+  if (!todos) return [];
+ let especie = species.filter((animal) => todos.includes(animal.id))
+ return especie
 }
+
+// console.log(getSpeciesByIds(
+//   '0938aa23-f153-4937-9f88-4858b24d6bce',
+//   'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+// ));
 
 module.exports = getSpeciesByIds;
