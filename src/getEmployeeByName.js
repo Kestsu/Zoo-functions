21244d9456316a8e1967f4c0a04938ecfbde1069
Kevin Li <Kevinstart14@gmail.com>
2022-04-17 12:54:59 -0300

    feito o 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  const resultado = employees.filter(
+    (nome) =>
+      employeeName.includes(nome.firstName) || employeeName.includes(nome.lastName),
+  );
+  return resultado[0];
 }
+
+// console.log(getEmployeeByName('Wishart'));
 
 module.exports = getEmployeeByName;
