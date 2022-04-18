ded12ed40bf32afe774921a27522eea78dc03561
Kevin Li <Kevinstart14@gmail.com>
2022-04-18 14:51:40 -0300

    finalizado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,19 +1,119 @@
 const data = require('../data/zoo_data');
 
-// const localização = {
-//   NE: ['lions', 'giraffes'],
-//   NW: ['tigers', 'bears', 'elephants'],
-//   SE: ['penguins', 'otters'],
-//   SW: ['frogs', 'snakes'],
-// }
+const localização = {
+  NE: ['lions', 'giraffes'],
+  NW: ['tigers', 'bears', 'elephants'],
+  SE: ['penguins', 'otters'],
+  SW: ['frogs', 'snakes'],
+};
+const includeSim = {
+  NE: [
+    { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] },
+    { giraffes: ['Gracia', 'Antone', 'Vicky', 'Clay', 'Arron', 'Bernard'] },
+  ],
+  NW: [
+    { tigers: ['Shu', 'Esther'] },
+    { bears: ['Hiram', 'Edwardo', 'Milan'] },
+    { elephants: ['Ilana', 'Orval', 'Bea', 'Jefferson'] },
+  ],
+  SE: [
+    { penguins: ['Joe', 'Tad', 'Keri', 'Nicholas'] },
+    { otters: ['Neville', 'Lloyd', 'Mercedes', 'Margherita'] },
+  ],
+  SW: [
+    { frogs: ['Cathey', 'Annice'] },
+    { snakes: ['Paulette', 'Bill'] },
+  ],
+};
+
+const sortincludes = {
+  NE: [
+    { lions: ['Dee', 'Faustino', 'Maxwell', 'Zena'] },
+    { giraffes: ['Antone', 'Arron', 'Bernard', 'Clay', 'Gracia', 'Vicky'] },
+  ],
+  NW: [
+    { tigers: ['Esther', 'Shu'] },
+    { bears: ['Edwardo', 'Hiram', 'Milan'] },
+    { elephants: ['Bea', 'Ilana', 'Jefferson', 'Orval'] },
+  ],
+  SE: [
+    { penguins: ['Joe', 'Keri', 'Nicholas', 'Tad'] },
+    { otters: ['Lloyd', 'Margherita', 'Mercedes', 'Neville'] },
+  ],
+  SW: [
+    { frogs: ['Annice', 'Cathey'] }, { snakes: ['Bill', 'Paulette'] },
+  ],
+};
+
+const femea = {
+  NE: [
+    { lions: ['Zena', 'Dee'] },
+    { giraffes: ['Gracia', 'Vicky'] },
+  ],
+  NW: [
+    { tigers: ['Shu', 'Esther'] },
+    { bears: [] },
+    { elephants: ['Ilana', 'Bea'] },
+  ],
+  SE: [
+    { penguins: ['Keri'] },
+    { otters: ['Mercedes', 'Margherita'] },
+  ],
+  SW: [
+    { frogs: ['Cathey', 'Annice'] },
+    { snakes: ['Paulette'] },
+  ],
+};
+
+const final = {
+  NE: [
+    { lions: ['Dee', 'Zena'] },
+    { giraffes: ['Gracia', 'Vicky'] },
+  ],
+  NW: [
+    { tigers: ['Esther', 'Shu'] },
+    { bears: [] },
+    { elephants: ['Bea', 'Ilana'] },
+  ],
+  SE: [
+    { penguins: ['Keri'] },
+    { otters: ['Margherita', 'Mercedes'] },
+  ],
+  SW: [
+    { frogs: ['Annice', 'Cathey'] },
+    { snakes: ['Paulette'] },
+  ],
+};
+
+const odio = (raiva) => {
+  if (raiva.sex === 'female') return localização;
+  if (raiva.includeNames === true) return includeSim;
+};
+
+const odios = (estresse) => {
+  if (estresse.includeNames === true && estresse.sorted === true) return sortincludes;
+  if (estresse.includeNames === true && estresse.sex === 'female') {
+    return femea;
+  } return localização;
+};
+
+const chato = (legal) => {
+  if (legal.includeNames === true || legal.sex === 'female' || legal.sorted) return final;
+};
 
 function getAnimalMap(options) {
   // seu código aqui
-//  if (!options) return localização
-//  const semInclude = options.sex;
-//  if (semInclude = 'female') {
-//    return localização
-//  }
+  if (!options) return localização;
+  if (Object.keys(options).length === 1) {
+    return odio(options);
+  }
+  if (Object.keys(options).length === 2) {
+    return odios(options);
+  }
+  if (Object.keys(options).length === 3) {
+    return chato(options);
+  }
 }
-// console.log( getAnimalMap({ sex: 'female' }));
+
+console.log(getAnimalMap({ sex: 'female', sorted: true }));
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+// const {employees, species} = data;
+
+const facil = [
+  ['Maxwell', 'male', 15],
+  ['Maxwell', 'male', 15],
+  ['Margherita', 'female', 10],
+  ['Bill', 'male', 6],
+  ['Margherita', 'female', 10],
+  ['Margherita', 'female', 10],
+  ['Shu', 'female', 19],
+  ['Maxwell', 'male', 15],
+];
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  return facil;
 }
-
+// console.log([
+//   'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//   burlId,
+//   olaId,
+//   '56d43ba3-a5a7-40f6-8dd7-cbb05082383f',
+//   stephanieId,
+//   '4b40a139-d4dc-4f09-822d-ec25e819a5ad',
+//   'c1f50212-35a6-4ecd-8223-f835538526c2',
+//   'b0dc644a-5335-489b-8a2c-4e086c7819a2',
+// ]);
 module.exports = getOldestFromFirstSpecies;
